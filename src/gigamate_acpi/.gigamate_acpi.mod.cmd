535d47c3ea50ad43d5db55675d3d89f6191b326f
adriansc <adriansc@adrianslaptop>
2026-07-08 23:43:04 +0200

    Clean up kernel module build artifacts from tracking

--- src/gigamate_acpi/.gigamate_acpi.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_gigamate_acpi.mod := printf '%s\n'   gigamate_acpi.o | awk '!x[$$0]++ { print("./"$$0) }' > gigamate_acpi.mod
--- src/gigamate_acpi/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo gigamate_acpi.o; :; } > modules.order
